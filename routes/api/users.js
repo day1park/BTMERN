c36b79eec5123ae1191e4d869a63c3012c161d2f
day1park <daypark2408@gmail.com>
2019-01-08 18:10:38 +1100

    create register route for user and yarn add gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 // authentication username email password etc
 const express = require("express");
 const router = express.Router();
+//gravat library
+const gravatar = require("gravatar");
+
+// Load User model
+const User = require("../../models/User");
 
 // @route GET api/users/test
 // @desc tests users route
@@ -10,5 +15,31 @@ router.get("/test", (req, res) =>
     msg: "users works"
   })
 );
+
+// @route GET api/users/register
+// @desc register a user
+// @access public
+router.post("/register", (req, res) => {
+  // use mongoose to find if email exists/ findOne is looking for a record access through req.body and then email // something with body-parser middleware
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm" // default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 
 module.exports = router;
