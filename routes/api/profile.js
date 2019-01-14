cf8edef3486788df50f4c5a6efa078dba25a7df8
day1park <daypark2408@gmail.com>
2019-01-14 17:28:47 +1100

    fix file name and create profile path

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,13 @@
 
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load profile Model
+const Profile = require("../../models/Profile");
+// Load User Profile
+const User = require("../../models/User");
 
 // @route GET api/profile/test
 // @desc tests profile route
@@ -10,6 +17,27 @@ router.get("/test", (req, res) =>
   res.json({
     msg: "profile works"
   })
+);
+
+// @route GET api/profile/
+// @desc get current users profile
+// @access private //protected route so use passport
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "there is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
 );
 
 module.exports = router;
