04e86591ff8a1ed626b116cbb3a260f31dcafd94
day1park <daypark2408@gmail.com>
2019-01-15 20:05:38 +1100

    create a DELETE route for education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -257,4 +257,28 @@ router.delete(
   }
 );
 
+// @route DELETE api/profile/education/:edu_id
+// @desc delete education from profile
+// @access private
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.edu_id);
+
+        // splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        // save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
