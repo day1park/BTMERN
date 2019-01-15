158243c170fce3a682a1b171c6ec5f3ffd9b11dc
day1park <daypark2408@gmail.com>
2019-01-15 19:57:13 +1100

    create DELETE route for experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -233,4 +233,28 @@ router.post(
   }
 );
 
+// @route DELETE api/profile/experience/:exp_id
+// @desc delete experience from profile
+// @access private
+router.post(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+
+        // splice out of array
+        profile.experience.splice(removeIndex, 1);
+
+        // save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
