621cb590e4b912c6621307856986b8e6fd407e52
day1park <daypark2408@gmail.com>
2019-01-18 20:30:29 +1100

    create validation for post input

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,7 +5,11 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// post model
 const Post = require("../../models/Post");
+
+// validation
+const validatePostInput = require("../../validation/post");
 
 // @route GET api/posts/test
 // @desc tests post route
@@ -23,10 +27,18 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // check validation
+    if (!isValid) {
+      // if any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
     const newPost = new Post({
       text: req.body.text,
       name: req.body.name,
-      avatar: req.body.name,
+      avatar: req.body.avatar,
       user: req.user.id
     });
 
