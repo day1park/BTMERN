2cbba3e069ddc60867d66bdfd75d748b9f4cbbb6
day1park <daypark2408@gmail.com>
2019-01-21 10:49:27 +1100

    fix navbar links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,9 +7,9 @@ class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
           <div className="container">
-            <a className="navbar-brand" href="landing.html">
+            <Link className="navbar-brand" to="/">
               DevLinks
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -22,10 +22,10 @@ class Navbar extends Component {
             <div className="collapse navbar-collapse" id="mobile-nav">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                  <a className="nav-link" href="profiles.html">
+                  <Link className="nav-link" to="/profiles">
                     {" "}
                     Developers
-                  </a>
+                  </Link>
                 </li>
               </ul>
 
