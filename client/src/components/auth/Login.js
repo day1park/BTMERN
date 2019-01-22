9a6dcece0b4cd679ec701449901f82c83ac6b475
day1park <daypark2408@gmail.com>
2019-01-22 15:53:38 +1100

    change onchange function to arrow function syntax

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -8,13 +8,13 @@ class Login extends Component {
       password: "",
       errors: {}
     };
-    this.onChange = this.onChange.bind(this);
+
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange(e) {
+  onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
   onSubmit(e) {
     e.preventDefault();
