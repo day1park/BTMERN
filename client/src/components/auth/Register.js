f203c9beda732820e39fcd10a46a064d77825ff4
day1park <daypark2408@gmail.com>
2019-01-22 16:13:12 +1100

    set error state to response data

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -29,7 +29,7 @@ class Register extends Component {
     axios
       .post("/api/users/register", newUser)
       .then(res => console.log(res.data))
-      .catch(err => console.log(err.response.data));
+      .catch(err => this.setState({ errors: err.response.data }));
   }
 
   render() {
