48bebba79b8be401fe2b8c25250ca293c32c0e00
day1park <daypark2408@gmail.com>
2019-01-23 16:46:51 +1100

    import test dispatch action type to authreducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,1 +1,10 @@
-// this file will contain register action
+import { TEST_DISPATCH } from "./types";
+
+// Register
+
+export const registeruser = userData => {
+  return {
+    type: TEST_DISPATCH,
+    payload: userData
+  };
+};
