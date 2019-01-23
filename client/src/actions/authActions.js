bf3b1ec514c94cb9b8a00d2314237391c1e19b99
day1park <daypark2408@gmail.com>
2019-01-23 18:04:49 +1100

    create loginUser action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -14,3 +14,23 @@ export const registerUser = (userData, history) => dispatch => {
       })
     ); // dispatch an action type called geterrors
 };
+
+// Login - get User token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // save to localstorage
+      const { token } = res.data;
+      // Set token to localstorage
+      localStorage.setItem("jwtToken", token); // only stores strings
+      // Set token to Auth header
+      setAuthToken(token);
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
