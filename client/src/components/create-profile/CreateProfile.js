3a03967417ffda5e4e0ebba93b76e4f1374e1de0
day1park <daypark2408@gmail.com>
2019-01-25 17:12:50 +1100

    add select options status to create profile form

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -27,8 +27,61 @@ class CreateProfile extends Component {
       instagram: "",
       errors: {}
     };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log("submit");
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
+    const { errors } = this.state;
+
+    // select options for status
+    const options = [
+      {
+        label: "* Select Professional Status",
+        value: 0
+      },
+      {
+        label: "Developer",
+        value: "Developer"
+      },
+      {
+        label: "Junior Developer",
+        value: "Junior Developer"
+      },
+      {
+        label: "Senior Developer",
+        value: "Senior Developer"
+      },
+      {
+        label: "Manager",
+        value: "Manager"
+      },
+      {
+        label: "Student or Learning",
+        value: "Student or Learning"
+      },
+      {
+        label: "Instructor or Teacher",
+        value: "Instructor or Teacher"
+      },
+      {
+        label: "Intern",
+        value: "Intern"
+      },
+      {
+        label: "Other",
+        value: "Other"
+      }
+    ];
     return (
       <div className="create-profile">
         <div className="container">
@@ -39,6 +92,25 @@ class CreateProfile extends Component {
                 please provide info to make your profile
               </p>
               <small className="d-block pb-3">* = required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile URL. you full name, company name, nickname"
+                />
+                <SelectListGroup
+                  placeholder="Status"
+                  name="status"
+                  value={this.state.status}
+                  onChange={this.onChange}
+                  options={options}
+                  error={errors.status}
+                  info="please select an option of what your career status is at"
+                />
+              </form>
             </div>
           </div>
         </div>
