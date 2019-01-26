8dede05ad0cc7d82a49dca597b9993af547302f0
day1park <daypark2408@gmail.com>
2019-01-26 20:23:39 +1100

    create add experience component form

---
+++ client/src/components/add-credentials/AddExperience.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { TextFieldGroup } from "../common/TextAreaFieldGroup";
+import { TextAreaFieldGroup } from "../common/TextAreaFieldGroup";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Subscription } from "rxjs";
+
+class AddExperience extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      company: "",
+      title: "",
+      location: "",
+      from: "",
+      to: "",
+      current: false,
+      description: "",
+      errors: {},
+      disabled: false
+    };
+  }
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div className="add-experience">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <Link to="/dashboard" className="btn btn-light">
+                Go Back
+              </Link>
+              <h1 className="display-4 text-center">Add Experience</h1>
+              <p className="lead text-center">
+                Add current job / position or that you have had in the past
+              </p>
+              <small className="d-block pb-3">* = Required fields</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+AddExperience.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps)(withRouter(AddExperience));
