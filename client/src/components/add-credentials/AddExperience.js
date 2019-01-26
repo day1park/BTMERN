d01aeea954dc9f91a7e91ea27484a649ffd183c0
day1park <daypark2408@gmail.com>
2019-01-26 20:42:20 +1100

    create onsubmit onchange and oncheck functions

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -20,7 +20,26 @@ class AddExperience extends Component {
       errors: {},
       disabled: false
     };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onCheck = this.onCheck.bind(this);
   }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log("submit");
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onCheck(e) {
+    this.setState({
+      disabled: !this.state.disabled,
+      current: !this.state.current
+    });
+  }
+
   render() {
     const { errors } = this.state;
 
