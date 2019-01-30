4bb95b5dbbb6b24cde4d1757d9ea39ca5c73675e
day1park <daypark2408@gmail.com>
2019-01-30 20:41:08 +1100

    add lifecycle method to to profiles and conditionals for displaying profile items or spinner

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -5,7 +5,23 @@ import Spinner from "../common/Spinner";
 import { getProfiles } from "../../actions/profileActions";
 
 class Profiles extends Component {
+  componentDidMount = () => {
+    this.props.getProfiles();
+  };
+
   render() {
+    const { profiles, loading } = this.profile;
+    let profileItems;
+
+    if (profiles === null || loading) {
+      profileItems = <Spinner />;
+    } else {
+      if (profiles.length > 0) {
+        <h1>Profiles will display here</h1>;
+      } else {
+        profileItems = <h4>No Profiles Found...</h4>;
+      }
+    }
     return (
       <div>
         <h1>Profiles Index</h1>
