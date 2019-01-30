307ed4428261667ac169d5d025bf2275c122ab2a
day1park <daypark2408@gmail.com>
2019-01-30 19:27:40 +1100

    map experience and render in a table

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -5,8 +5,32 @@ import { withRouter } from "react-router-dom";
 
 class Experience extends Component {
   render() {
+    const experience = this.props.experience.map(exp => (
+      <tr key={exp._id}>
+        <td>{exp.company}</td>
+        <td>{exp.title}</td>
+        <td>
+          {exp.from} - {exp.to}
+        </td>
+        <td>
+          <button className="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    ));
     return (
       <div>
+        <h4 className="mb-4">Experience Credentials</h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Title</th>
+              <th>Years</th>
+              <th>Years</th>
+            </tr>
+          </thead>
+          <tbody>{experience}</tbody>
+        </table>
         <h1> experience component</h1>
       </div>
     );
