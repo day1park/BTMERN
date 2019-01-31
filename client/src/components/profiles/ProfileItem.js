166d6bf0eb3b1337325e763fb56cc01a5a968dc0
day1park <daypark2408@gmail.com>
2019-01-31 14:53:26 +1100

    import profileitems component to render in profiles component

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import isEmpty from "../../validation/is-empty";
 
 class ProfileItem extends Component {
