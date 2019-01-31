b13ec9509a9ff31ac3986fdba0b4758c8edc8416
day1park <daypark2408@gmail.com>
2019-01-31 18:22:32 +1100

    add template post form for postform component

---
+++ client/src/components/posts/PostForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import { addPost } from "../../actions/postActions";
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      errors: {}
+    };
+  }
+  render() {
+    return (
+      <div className="post-form mb-3">
+        <div className="card card-info">
+          <div className="card-header bg-info text-white">Say Somthing...</div>
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <textarea
+                  className="form-control form-control-lg"
+                  placeholder="Create a post"
+                />
+              </div>
+              <button type="submit" className="btn btn-dark">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PostForm;
