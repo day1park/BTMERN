b1ecdb0f1769a901975e1757f6b12b1991001075
day1park <daypark2408@gmail.com>
2019-01-31 14:21:28 +1100

    create profile item class component

---
+++ client/src/components/profiles/ProfileItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProfileItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">profile item</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileItem;
