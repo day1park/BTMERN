ee1db329951baa6bfcad2cb311f92486db31d76c
day1park <daypark2408@gmail.com>
2019-01-31 17:58:40 +1100

    create postReducer

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,12 @@
+const initalState = {
+  posts: [],
+  post: {},
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
