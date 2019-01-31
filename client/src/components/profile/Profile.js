ba10d4f60f84f8e0e9a23f1af1d60b74f0ca8ab5
day1park <daypark2408@gmail.com>
2019-01-31 15:09:28 +1100

    import sub components into Profile component

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import ProfileHeader from "./ProfileHeader";
+import ProfileAbout from "./ProfileAbout";
+import ProfileCreds from "./ProfileCreds";
+import ProfileGithub from "./ProfileGithub";
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <h1> profile about</h1>
+      </div>
+    );
+  }
+}
+
+export default Profile;
