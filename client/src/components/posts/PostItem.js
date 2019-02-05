5bb38cd0ebcac35ebf9c300cabd25cdf76d5b6e9
day1park <daypark2408@gmail.com>
2019-02-05 14:22:02 +1100

    map through postitems in post feed component and display delete button

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -5,11 +5,48 @@ import classnames from "classnames";
 import { Link } from "react-router-dom";
 
 class PostItem extends Component {
+  onDeleteClick(id) {
+    console.log(id);
+  }
   render() {
     const { post, auth } = this.props;
     return (
-      <div>
-        <h1>post item</h1>
+      <div className="card card-body mb-3">
+        <div className="row">
+          <div className="col-md-2">
+            <a href="profile.html">
+              <img
+                className="rounded-circle d-none d-md-block"
+                src={post.avatar}
+                alt=""
+              />
+            </a>
+            <br />
+            <p className="text-center">{post.name}</p>
+          </div>
+          <div className="col-md-10">
+            <p className="lead">{post.text}</p>
+            <button type="button" className="btn btn-light mr-1">
+              <i className="text-info fas fa-thumbs-up" />
+              <span className="badge badge-light">{post.likes.length}</span>
+            </button>
+            <button type="button" className="btn btn-light mr-1">
+              <i className="text-secondary fas fa-thumbs-down" />
+            </button>
+            <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
+              Comments
+            </Link>
+            {post.user === auth.user.id ? (
+              <button
+                onClick={this.onDeleteClick.bind(this, post._id)}
+                type="button"
+                className="btn btn-danger mr-1"
+              >
+                <i className="fas fa-times" />
+              </button>
+            ) : null}
+          </div>
+        </div>
       </div>
     );
   }
